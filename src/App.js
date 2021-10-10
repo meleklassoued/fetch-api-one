fdeed12a7a600c7d725d0e89f64e732751d7752c
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-10-11 00:45:37 +0100

    fetching an API with success

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-
+import React from "react";
+import Axios from "axios";
 import "./App.css";
 
 function App() {
-  return <h1>aslema</h1>;
+  const getJoke = () => {
+    Axios.get("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => {
+        console.log(response);
+        
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <>
+      <h1>aslema</h1>
+      <div>
+        <button onClick={getJoke}>click Me</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
